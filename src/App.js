0e740ed4822499b36139597ae1f1c81bdc74c41a
vlowe85 <vince@vlowe.co.uk>
2021-06-17 16:48:33 +0100

    Added a header to look "branded", not spending too long on this, would usually follow a design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.scss'
+import Header from './Header'
 import SearchForm from './SearchForm'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <main className="o-container">
         <div className="o-layout u-margin-top-large">
           <div className="o-layout__item">
---
+++ src/Header.js
@@ -0,0 +1,17 @@
+const Header = () => {
+    return (
+      <header className="c-header">
+        <div className="o-container o-container--wide">
+          <div className="o-layout o-layout--middle">
+            <div className="o-layout__item">
+              <a href="/" className="c-header__logo">
+                <span className="u-hide-visually">Sky</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  }
+   
+  export default Header;
